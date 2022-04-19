5e35e5ee20221e0b29050275afe60a04733aa20f
shreder0804 <shreder0804@gmail.com>
2022-04-19 22:31:13 +0900

    Add : 01_05_declareFunction.js

---
+++ coplit/01_05_declareFunction.js
@@ -0,0 +1,32 @@
+function multiplyBy2(input) {
+  input = input * 2;
+  return input;
+}
+
+// TODO : 첫 번째 인자에 2를 곱하는 함수 multiplyBy2와 비슷하게 첫 번째 인자를 2로 나누는 함수 divideBy2를 선언하세요.
+
+function divideBy2(input) {
+  input = input / 2;
+  return input;
+}
+
+// 자바스크립트에서 함수는 두가지 방식으로 선언될 수 있습니다. 첫 번째 함수 `plusOne`
+// 은 함수 선언식입니다. 두 번째 함수 `minusOne`은 함수 표현식을 변수 `minusOne`에 할당했습니다. 미묘한 차이는 있지만 **변수를 선언하고, 함수를 할당했다**라는 결론은 분명합니다.
+
+// ```jsx
+// function plusOne(input1) {
+//   input1 = input1 + 1;
+//   return input1;
+// }
+
+// let minusOne = function (input2) {
+//   input2 = input2 - 1;
+//   return input2;
+// };
+// ```
+
+// 그렇다면 이번에는 곱셈과 나눗셈을 하는 함수를 만들어볼까요?
+
+// ## **문제**
+
+// 첫 번째 인자에 2를 곱하는 함수 multiplyBy2와 비슷하게 첫 번째 인자를 2로 나누는 함수 divideBy2를 선언하세요.
