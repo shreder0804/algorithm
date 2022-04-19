2cc7085a261140aacdce4b10d2ae3452e55bb3e8
shreder0804 <shreder0804@gmail.com>
2022-04-20 08:57:32 +0900

    Fix : Add question

--- coplit/02_conditional/02_04_isGreaterThan.js
+++ coplit/02_conditional/02_04_isGreaterThan.js
@@ -5,3 +5,34 @@ function isGreaterThan(num1, num2) {
   }
   return false;
 }
+
+// # **isGreaterThan**
+
+// ## **문제**
+
+// 두 개의 수를 입력받아 두번째 수가 첫번째 수보다 큰지 여부를 리턴해야 합니다.
+
+// ## **입력**
+
+// ### **인자 1 : num1**
+
+// - `number` 타입의 수
+
+// ### **인자 2 : num2**
+
+// - `number` 타입의 수
+
+// ## **출력**
+
+// - `boolean` 타입을 리턴해야 합니다.
+
+// ## **주의 사항**
+
+// - 같은 수를 입력받은 경우, `false`를 리턴해야 합니다
+
+// ## **입출력 예시**
+
+// ```jsx
+// let output = isGreaterThan(11, 10);
+// console.log(output); // --> false
+// ```
