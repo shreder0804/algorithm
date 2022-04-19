7c196da9e2e529a76ee236be61dac6b5e519d737
shreder0804 <shreder0804@gmail.com>
2022-04-19 23:09:12 +0900

    Add : 02_03_equalsTen.js

---
+++ coplit/02_conditional/02_03_equalsTen.js
@@ -0,0 +1,27 @@
+function equalsTen(num) {
+  // TODO: 여기에 코드를 작성합니다.
+  return num === 10 ? true : false;
+}
+
+// # **equalsTen**
+
+// ## **문제**
+
+// 수를 입력받아 10과 같은지 여부를 리턴해야 합니다.
+
+// ## **입력**
+
+// ### **인자 1 : num**
+
+// - `number` 타입의 수
+
+// ## **출력**
+
+// - `boolean` 타입을 리턴해야 합니다.
+
+// ## **입출력 예시**
+
+// ```jsx
+// let output = equalsTen(9);
+// console.log(output); // --> false
+// ```
