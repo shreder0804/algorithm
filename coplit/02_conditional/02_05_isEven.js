793a32577e0ae97b256fadcdb19a3e68ed443a54
shreder0804 <shreder0804@gmail.com>
2022-04-20 08:58:05 +0900

    Add : 02_05_isEven.js

---
+++ coplit/02_conditional/02_05_isEven.js
@@ -0,0 +1,33 @@
+function isEven(num) {
+  // TODO: 여기에 코드를 작성합니다.
+  return num % 2 === 0 ? true : false;
+}
+
+//   # **isEven**
+
+// 수를 입력받아 짝수인지 여부를 리턴해야 합니다.
+
+// ## **입력**
+
+// ### **인자 1 : num**
+
+// - `number` 타입의 정수 (`num` >= 0)
+
+// ## **출력**
+
+// - `boolean` 타입을 리턴해야 합니다.
+
+// ## **주의 사항**
+
+// - 0은 짝수로 간주합니다.
+
+// ## **입출력 예시**
+
+// ```jsx
+// let output = isEven(11);
+// console.log(output); // --> false
+// ```
+
+// ## **힌트**
+
+// - 짝수(even number)와 홀수(odd number)의 차이를 검색해 봅니다. (`js check even number`)
