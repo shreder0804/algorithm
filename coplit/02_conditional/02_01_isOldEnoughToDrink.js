e9b91e96465f9b988c35e48426ce159d813a314d
shreder0804 <shreder0804@gmail.com>
2022-04-19 22:56:39 +0900

    Add : 02_01_isOldEnoughToDrink.js

---
+++ coplit/02_conditional/02_01_isOldEnoughToDrink.js
@@ -0,0 +1,28 @@
+function isOldEnoughToDrink(age) {
+  // TODO: 여기에 코드를 작성합니다.
+  return age >= 18 ? true : false;
+}
+
+// # **isOldEnoughToDrink**
+
+// ## **문제**
+
+// 나이를 입력받아 술을 마실 수 있는지(18세 이상) 여부를 리턴해야 합니다.
+
+// ## **입력**
+
+// ### **인자 1 : age**
+
+// - `number` 타입의 나이 (0 이상의 정수)
+
+// ## **출력**
+
+// - `boolean` 타입을 리턴해야 합니다.
+
+// ## **입출력 예시**
+
+// ```jsx
+// function isOldEnoughToDrink(age) {
+//   // TODO: 여기에 코드를 작성합니다.
+// }
+// ```
