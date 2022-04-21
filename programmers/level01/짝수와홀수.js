939de7f0d80877a0bce0989eee276b852ef1c04b
shreder0804 <shreder0804@gmail.com>
2022-04-21 19:24:12 +0900

    Add : 짝수와홀수.js

---
+++ programmers/level01/짝수와홀수.js
@@ -0,0 +1,8 @@
+function solution(num) {
+  var answer = '';
+  // num이 짝수이면 "Even" 반환
+  // num이 홀수이면 "Odd" 반환
+  return num % 2 === 0 ? (answer = 'Even') : (answer = 'Odd');
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12937
