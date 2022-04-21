02575772b89442a7f70a45932295ad7dc8df2901
shreder0804 <shreder0804@gmail.com>
2022-04-21 21:24:16 +0900

    Add : 내적.js

---
+++ programmers/level01/내적.js
@@ -0,0 +1,8 @@
+function solution(a, b) {
+  var answer = 0;
+  // for문을 사용하여 a의 배열 요소와 b의 배열 요소를 곱한 값을 answer에 더한다.
+  for (let i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
+  return answer;
+}
