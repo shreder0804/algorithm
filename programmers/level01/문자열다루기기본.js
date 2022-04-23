bcbfaa100045f479b8947f2a82a800ec5080895e
shreder0804 <shreder0804@gmail.com>
2022-04-23 22:57:40 +0900

    Fix : Delete isNaN() function

--- programmers/level01/문자열다루기기본.js
+++ programmers/level01/문자열다루기기본.js
@@ -1,7 +1,7 @@
 function solution(s) {
   var answer = true;
   // 문자열의 길이가 4 또는 6이고 숫자로만 구성되어 있는지 확인
-  (s.length === 4 || s.length === 6) && !isNaN(s) ? (answer = true) : (answer = false);
+  (s.length === 4 || s.length === 6) && s == parseInt(s) ? (answer = true) : (answer = false);
   return answer;
 }
 
