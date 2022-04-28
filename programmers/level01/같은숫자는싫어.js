173140ca6b383afe90d65db27355e222ce2369b8
shreder0804 <shreder0804@gmail.com>
2022-04-28 13:09:34 +0900

    Fix : Add comment

--- programmers/level01/같은숫자는싫어.js
+++ programmers/level01/같은숫자는싫어.js
@@ -3,6 +3,7 @@ function solution(arr) {
   // 초기 num 변수에 undefined 하랑
   let num = undefined;
   // for문을 사용하여 num의 숫자와 다르면 arr[i]를 넣고 같으면 넣지 않는다.
+  // 삼항연산자를 두줄 이상으로 쓸 경우엔 아래와 같이 괄호로 묶어줌
   for (let i = 0; i < arr.length; i++) {
     num != Number(arr[i]) ? ((num = arr[i]), answer.push(arr[i])) : null;
   }
