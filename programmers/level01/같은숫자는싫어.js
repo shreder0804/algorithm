870240007d6b33c601bde9add00361807500c4fc
shreder0804 <shreder0804@gmail.com>
2022-04-28 13:08:15 +0900

    Refactor : Change if statement to ternary operator

--- programmers/level01/같은숫자는싫어.js
+++ programmers/level01/같은숫자는싫어.js
@@ -4,10 +4,7 @@ function solution(arr) {
   let num = undefined;
   // for문을 사용하여 num의 숫자와 다르면 arr[i]를 넣고 같으면 넣지 않는다.
   for (let i = 0; i < arr.length; i++) {
-    if (num != Number(arr[i])) {
-      num = arr[i];
-      answer.push(arr[i]);
-    }
+    num != Number(arr[i]) ? ((num = arr[i]), answer.push(arr[i])) : null;
   }
   return answer;
 }
