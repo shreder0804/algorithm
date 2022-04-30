c442e4f4afefcacfb6f7b3b56be5840e502110d1
shreder0804 <shreder0804@gmail.com>
2022-04-30 22:04:35 +0900

    Add : 나머지가1이되는수찾기.js

---
+++ programmers/level01/나머지가1이되는수찾기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  var answer = 0;
+  // for문을 이용하여 자연수 1부터 순서대로 반복하여 나머지가 1이 되면 리턴
+  for (let i = 1; i < n; i++) {
+    if (n % i === 1) {
+      answer = i;
+      break;
+    }
+  }
+  return answer;
+}
