f09d13884491e345b5fe4c94b64316bbba1e56db
shreder0804 <shreder0804@gmail.com>
2022-05-04 12:33:39 +0900

    Fix : Update code

--- programmers/level01/최대공약수와최소공배수.js
+++ programmers/level01/최대공약수와최소공배수.js
@@ -1,24 +1,30 @@
 function solution(n, m) {
+  // 변수 초기화
   var answer = [];
   let divisorN = [];
   let divisorM = [];
   let gcdArr = [];
+  let gcd = 0;
+
   // n과 m의 약수 구하기
   for (let i = 1; i <= n; i++) n % i === 0 ? divisorN.push(i) : null;
   for (let j = 1; j <= m; j++) m % j === 0 ? divisorM.push(j) : null;
-  console.log(divisorN);
-  console.log(divisorM);
 
   // 약수 배열의 교집합 구하기
   gcdArr = divisorN.filter((ele) => divisorM.includes(ele));
 
+  // 최대공약수 할당
+  gcd = gcdArr[gcdArr.length - 1];
+
   // gcdArr의 가장 큰 수가 '최대공약수'이기 때문에
   // gcdArr의 마지막 요소를 answer의 첫번째 요소에 push
-  answer.push(gcdArr[gcdArr.length - 1]);
+  answer.push(gcd);
 
-  console.log(gcdArr);
+  // 최소공배수는 유클리드 호제법을 이용하여 n과 m의 곱을
+  // 최대공약수로 나눠서 구하며, 구한 값을 answer에 push
+  answer.push((n * m) / gcd);
 
   return answer;
 }
 
-console.log(solution(50, 4));
+// https://programmers.co.kr/learn/courses/30/lessons/12940
