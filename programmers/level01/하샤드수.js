a987a068b566564893cd2069d424d3eb05fe14ad
shreder0804 <shreder0804@gmail.com>
2022-05-10 12:34:18 +0900

    Add : 하샤드수.js

---
+++ programmers/level01/하샤드수.js
@@ -0,0 +1,23 @@
+function solution(x) {
+  var answer = true;
+  let arr = [];
+  let total = 0;
+  // x를 문자열화 하여 배열로 만들어 arr에 할당
+  arr = String(x).split("");
+
+  // arr의 합을 total에 할당
+  for (let i = 0; i < arr.length; i++) total += Number(arr[i]);
+
+  // x를 total로 나누어서 나머지가 0이면 true, 0이 아니면 false
+  x % total === 0 ? (answer = true) : (answer = false);
+
+  return answer;
+}
+
+// https://programmers.co.kr/learn/courses/30/lessons/12947
+console.log(solution(10));
+
+let num = 10;
+let arr02 = String(num).split("");
+
+console.log(arr02);
