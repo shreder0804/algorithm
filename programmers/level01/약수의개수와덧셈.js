b70a4554005c261c2e8a70094995762f8081d074
shreder0804 <shreder0804@gmail.com>
2022-05-12 12:35:11 +0900

    Fix : Modify comment

--- programmers/level01/약수의개수와덧셈.js
+++ programmers/level01/약수의개수와덧셈.js
@@ -4,14 +4,12 @@ function solution(left, right) {
   // left부터 right까지 반복문 실행
   for (let i = left; i <= right; i++) {
     // j의 약수 구하기
-    for (let j = 1; j <= i; j++) {
-      i % j === 0 ? divisor.push(j) : null;
-    }
-    // 해당 요소의 약수의 갯수를 구하고 갯수가 짝수이면
-    // answer에 더하고 홀수면 뺀다
+    for (let j = 1; j <= i; j++) i % j === 0 ? divisor.push(j) : null;
+
+    // 해당 요소의 약수의 갯수를 구하고 갯수가 짝수면 answer에 i를 더하고 홀수면 i를 뺌
     divisor.length % 2 === 0 ? (answer += i) : (answer -= i);
 
-    // divisor 모든 요소 삭제
+    // divisor 배열 초기화
     divisor = [];
   }
 
